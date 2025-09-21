34695287aea08628d4ced77ee99395f1d5c122cc
Matas Michailinas <matasmichailinas@gmail.com>
2025-09-21 14:38:19 +0200

    Initial commit. (already have a window class with redraw method defined)

---
+++ graphics.py
@@ -0,0 +1,16 @@
+from tkinter import Tk, BOTH, Canvas
+
+Class Window:
+    def __init__(self, width, height):
+            self.width = width
+            self.height = height
+            self.__root = Tk()
+            self.__root.title("Maze Solver")
+            self.__canvas = Canvas(self.__root, width=self.width, height=self.height)
+            self.__canvas.pack(fill=BOTH)
+            self.running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+        
